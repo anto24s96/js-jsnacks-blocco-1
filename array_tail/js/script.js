5e78313b3a5163fbbf8200c1d56120cf89430b8e
anto24s96 <anto24s96@gmail.com>
2023-11-24 18:26:07 +0100

    [BONUS] exercise completed

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -2,6 +2,9 @@
 let array = [];
 /* CHIEDERE ALL'UTENTE IL NUMERO DI ELEMENTI */
 let user_n_elements = parseInt(prompt("Inserisci il numero di elementi desiderato"));
+/* CHIEDERE ALL'UTENTE QUANTI ELEMENTI DELL'ARRAY VUOLE STAMPARE */
+let n_elements = parseInt(prompt("Inserisci il numero di elementi che vuoi stampare"));
+
 /* CREARE UN CICLO PER GENERARE I NUMERI RANDOMICI */
 for(let i = 0; i < user_n_elements; i++){
     array.push(Math.floor(Math.random() * 100) + 1);
@@ -10,3 +13,11 @@ for(let i = 0; i < user_n_elements; i++){
 console.log(array);
 /* STAMPO SOLO GLI ULTIMI 5 */
 console.log(array.slice(-5));
+
+/* VERIFICARE GLI ELEMENTI IN CODA */
+if(n_elements <= user_n_elements){
+    console.log("Stampa in corso...");
+}
+else{
+    console.log("Richiesta non valida!");
+}
